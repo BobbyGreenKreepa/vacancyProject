060f387ea530004427bcd9a9af7367463d139a89
romanvasiljev <koksenvai@gmailc.om>
2024-01-22 23:15:15 +0500

    Add demand, vacancies parser and cb api enjoy, edit base css and ad demand html view and auto graphic builder

--- application/views/main.py
+++ application/views/main.py
@@ -6,8 +6,6 @@ from application.domain.Vacancy import Vacancy
 def main_view(request):
     vacancies = Vacancy.objects.all()
     context = {'data': vacancies}
-    for i in vacancies:
-        print(i.image)
     return render(
         request,
         'application/main.html',
